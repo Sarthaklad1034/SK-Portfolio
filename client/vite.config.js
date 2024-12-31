2f508c13517c936b7f35b508bc135825a348f07c
Sarthak <sarthaklad1034@gmail.com>
2024-12-31 12:49:49 +0530

    Updated @ alias for imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,11 @@ import path from 'path'
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, 'src') }
+        ]
     },
     build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'index.html')
-            }
-        }
+        outDir: 'dist'
     }
 })
