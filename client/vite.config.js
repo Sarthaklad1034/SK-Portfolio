c233c9845346c6c99e84c6d2505ae742670ba368
Sarthak <sarthaklad1034@gmail.com>
2024-12-31 13:48:31 +0530

    various changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: [
-            { find: '@', replacement: path.resolve(__dirname, 'src') }
-        ]
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-    build: {
-        outDir: 'dist'
-    }
 })
